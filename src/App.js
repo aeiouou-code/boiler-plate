70564a1bc3ec8e20f846078537754d183e80f4fe
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2021-10-26 23:32:13 +0900

    Add: eslint prettier 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
